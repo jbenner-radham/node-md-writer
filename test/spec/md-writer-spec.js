4fea04897a6934dbb1fa5f23dcbb63c2a0a52288
James Benner <james.benner@gmail.com>
2017-09-29 17:50:45 -0400

    Added specs for h1 and h2.

--- test/spec/md-writer-spec.js
+++ test/spec/md-writer-spec.js
@@ -3,8 +3,28 @@
 const any = jasmine.any;
 const mdWriter = require('../../');
 
-describe('readme-md', function () {
+describe('md-writer', function () {
     it('is a function', function () {
         expect(mdWriter).toEqual(any(Function));
+    });
+
+    describe('.h1', function () {
+        it('is a function', function () {
+            expect(mdWriter.h1).toEqual(any(Function));
+        });
+
+        it('returns a string', function () {
+            expect(mdWriter.h1()).toEqual(any(String));
+        });
+    });
+
+    describe('.h2', function () {
+        it('is a function', function () {
+            expect(mdWriter.h2).toEqual(any(Function));
+        });
+
+        it('returns a string', function () {
+            expect(mdWriter.h2()).toEqual(any(String));
+        });
     });
 });
