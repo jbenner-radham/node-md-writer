45df3931ddb2a112b87ec0573321d2abb1790ea8
James Benner <james.benner@gmail.com>
2022-12-29 16:00:54 -0500

    Add TypeScript types

---
+++ lib/index.d.ts
@@ -0,0 +1,21 @@
+export const LF: '\n';
+
+export function codeFence(infoString?: string): string;
+
+export function codeSpan(code?: string): string;
+
+export function fencedCodeBlock(code?: string, infoString?: string): string;
+
+export function fencedJsCodeBlock(code?: string): string;
+
+export function fencedTsCodeBlock(code?: string): string;
+
+export function fencedShCodeBlock(code?: string): string;
+
+export function h1(text?: string): string;
+
+export function h2(text?: string): string;
+
+export function h3(text?: string): string;
+
+export function h4(text?: string): string;
